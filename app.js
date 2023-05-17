ffd737bc9c3bbe2f803492a435f72d952e231580
jrSergey <morozov98.38.25@gmail.com>
2023-05-17 19:44:57 +0500

    feat: added code to change window

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 window.addEventListener('scroll', e => {
-	document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`) // Update method
+	//document.body.style.cssText += `--scrollTop: ${this.scrollY}px`
+	document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`) // Новый метод
 })
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
 ScrollSmoother.create({
 	wrapper: '.wrapper',
 	content: '.content'
 })
+
+const mainBox = document.querySelector('.main-box');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+
+
+registerLink.addEventListener('click', (e) => {
+    mainBox.classList.add('active');
+	e.preventDefault();
+});
+
+loginLink.addEventListener('click', (e) => {
+    mainBox.classList.remove('active');
+	e.preventDefault();
+});
+
+
